4b5a8a0cd97eae1b2c6a4b66c118b0f86f95b4d8
Artyom <a.Khodas1601@gmail.com>
2022-10-10 19:32:05 +0300

    FirstStreames

---
+++ src/Streams/Main.java
@@ -0,0 +1,22 @@
+package Streams;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("one");
+        list.add("two");
+
+        /*Stream<String> stream = list.stream();
+        stream.forEach(x-> System.out.println(x));*/
+        list.stream().forEach(x-> System.out.println(x));
+        Set<String> set = list.stream().filter(x -> x.equals("one")).collect(Collectors.toSet());
+        list.stream().map(x->x.toUpperCase());
+
+
+    }
+
+}
